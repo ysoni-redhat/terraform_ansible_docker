3c13f80841f43bf390f7308c5e6a0992feba8011
Yogita Soni <ysoni@redhat.com>
2025-02-27 15:49:38 +0530

    content updated

--- main.tf
+++ main.tf
@@ -2,63 +2,10 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# Create a Public VPC
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "MyPublicVPC"
-  }
-}
-
-# Create a public subnet
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
-
-  tags = {
-    Name = "PublicSubnet"
-  }
-}
-
-# Create an Internet Gateway
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "MyInternetGateway"
-  }
-}
-
-# Create a route table for public access
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "PublicRouteTable"
-  }
-}
-
-# Associate the public subnet with the public route table
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
 # Create a security group for SSH and HTTP access
 resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
+  name        = "allow_ssh_http_web"
   description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 22
@@ -111,27 +58,22 @@ resource "aws_instance" "my_instance" {
   ami                    = "ami-0ddfba243cbee3768"  # Update with the correct AMI ID
   instance_type          = "t2.micro"
   key_name               = "terraform"
-  subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
   associate_public_ip_address = true
-
   tags = {
     Name = "MyPublicEC2Instance"
   }
 }
 
 # Run Ansible Playbook after instance creation
-resource "null_resource" "ansible_provision" {
-  depends_on = [aws_instance.my_instance]
-
-  provisioner "local-exec" {
-    command = <<EOT
-      sleep 25  # Give some time for SSH service to start
-      ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -e "ansible_python_interpreter=/usr/bin/python3" -i "${aws_instance.my_instance.public_ip}," --private-key terraform.pem install_docker.yml
-
-    EOT
-  }
-}
+# resource "null_resource" "ansible_provision" {
+#  depends_on = [aws_instance.my_instance]
+#  provisioner "local-exec" {
+#    command = <<EOT
+#    ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user -e "ansible_python_interpreter=/usr/bin/python3" -i "${aws_instance.my_instance.public_ip}," --private-key terraform.pem install_docker.yml
+#    EOT
+#  }
+#}
 
 # Output the public IP
 output "public_ip" {
